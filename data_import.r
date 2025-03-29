be7e63553cbdd8f4adae5bd344e6c66d607f6f30
statgeek <fkhurshed@gmail.com>
2025-03-28 20:14:53 -0600

    Update data_import.r
    update for correct data and add data cleaning steps

--- data_import.r
+++ data_import.r
@@ -1,33 +1,51 @@
-#set location for files saved
 setwd("~/Documents/D4G")
 
-#import libraries
 library(tidyverse)
-library(readxl)
+
+#import all reports raw data
+call_reports_20_23 <- read_csv("./2020-2023/CallReports.csv")
+call_reports_20_23_links <- read_csv("./2020-2023/CallReportsLinkedTogether.csv", skip = 2)
+
+call_reports_24 <- read_csv("./2024/Call Reports.csv")
+call_reports_24_links <- read_csv("./2024/CallReportsLinkedTogether.csv")
 
 
-#import crisis raw data deleting extra columns
-crisis_2015_2016 <- read_xlsx("Crisis and CT Raw data 2015-2016.xlsx", skip=2)[,1:105]
+call_data <- merge(call_reports_20_23, call_reports_24, all=TRUE, sort=FALSE)
 
-crisis_2017_2018 <- read_xlsx("Crisis and CT Raw data 2017-2018.xlsx", skip=2)[,1:114]
-
-crisis_2019 <- read_xlsx("Crisis and CT Raw data 2019 Old Call Form.xlsx", skip=2)[,1:114]
-
-crisis_2020 <- read_xlsx("Crisis and CT Raw data 2020.xlsx", skip=2)
-
-crisis_2021_2022 <- read_xlsx("Crisis and CT Raw data 2021-2022.xlsx", skip=2)[,1:125]
-
-crisis_2023_2024 <- read_xlsx("Crisis and CT Raw data 2023-2024.xlsx", skip=2)[,1:124]
-
-#put names into lists with filenames
-names_1516<- as.data.frame(names(crisis_2015_2016)) %>% add_column(Years = "2015 & 2016", Indicator=1) %>% rename( 'names' =`names(crisis_2015_2016)`) 
-names_1718<- as.data.frame(names(crisis_2017_2018)) %>% add_column(Years = "2017 & 2018", Indicator=1) %>% rename( 'names' =`names(crisis_2017_2018)`)
-names_19<- as.data.frame(names(crisis_2019)) %>% add_column(Years = "2019", Indicator=1) %>% rename( 'names' =`names(crisis_2019)`)
-names_20<- as.data.frame(names(crisis_2020)) %>% add_column(Years = "2020", Indicator=1) %>% rename( 'names' =`names(crisis_2020)`)
-names_2122<- as.data.frame(names(crisis_2021_2022)) %>% add_column(Years = "2021 & 2022", Indicator=1) %>% rename( 'names' =`names(crisis_2021_2022)`)
-names_2324<- as.data.frame(names(crisis_2023_2024)) %>% add_column(Years = "2023 & 2024", Indicator=1) %>% rename( 'names' =`names(crisis_2023_2024)`)
+names_20_23<- as.data.frame(names(call_reports_20_23)) %>% add_column(Years = "2020-2023", Indicator=1) %>% rename( 'names' =`names(call_reports_20_23)`) 
+names_24<- as.data.frame(names(call_reports_24)) %>% add_column(Years = "2024", Indicator=1) %>% rename( 'names' =`names(call_reports_24)`)
 
 
 #combine names list into one and transpose to identify which variables are in which files
-variable_list <- rbind(names_1516, names_1718, names_19, names_20, names_2122, names2324) %>%
+variable_list <- rbind(names_20_23, names_24) %>%
     pivot_wider(id_cols = "names", names_from = "Years", values_from = "Indicator", values_fill = 0)
+
+#Report Versions
+rep_version_list1<- as_tibble(unique(call_reports_20_23$ReportVersion))
+rep_version_list2<- as_tibble(unique(call_reports_24$ReportVersion))
+
+rep_list <- as_tibble(unique(rbind(rep_version_list1, rep_version_list2)))
+
+
+unique_list <- function(var) {
+    summary_list <- as_tibble(unique(var))
+    return(summary_list)
+}
+
+#no issue
+Safety169 <- unique_list(call_data$`Safety and Rescue  - Resources (PIE) - Did you discuss?`)
+Safety168 <- unique_list(call_data$`Safety and Rescue  - Prior History`)
+Safety167 <- unique_list(call_data$`Safety and Rescue  - Pain and Present Acute State`)
+Safety166 <- unique_list(call_data$`Safety and Rescue  - Is the PAR presently safe?`)
+Safety165 <- unique_list(call_data$`Safety and Rescue  - Explore...148`)
+Safety164 <- unique_list(call_data$`Safety and Rescue  - Explore...147`)
+Safety163 <- unique_list(call_data$`Safety and Rescue  - Does the PAR have a suicide plan with accessible means?`)
+Safety162 <- unique_list(call_data$`Safety and Rescue  - Did you offer a follow up?`)
+Safety161 <- unique_list(call_data$`Safety and Rescue  - Did you define uncertainty?`)
+Safety160 <- unique_list(call_data$`Safety and Rescue  - Can the PAR stay safe for the duration of the call?`)
+
+ThirdParty159 <- unique_list(call_data$`Third Party  - Suicide in progress`)
+ThirdParty158 <- unique_list(call_data$`Third Party  - Have you thought about suicide in the last two months?`)
+ThirdParty157 <- unique_list(call_data$`Third Party  - Have you ever attempted to kill yourself?`)
+ThirdParty156<- unique_list(call_data$`Third Party  - Are you thinking of suicide?`)
+
